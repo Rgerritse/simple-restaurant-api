6ea5a045cb6e49421a960d484d53e59aae92e985
Ruben <rgerritse95@gmail.com>
2022-05-14 13:17:06 +0900

    fn add_items

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::collections::HashMap;
+
+fn add_items(orders: &mut HashMap<u8, Vec<(u8, u8)>>, table: u8, items: &[(u8, u8)]) {
+    match orders.get_mut(&table) {
+        Some(v) => {
+            for item in items { v.push(*item) }
+        }
+        None => {
+            orders.insert(table, items.to_vec()); 
+        }
+    };
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut orders: HashMap<u8, Vec<(u8, u8)>> = HashMap::new();
+    
+    add_items(&mut orders, 1, &[(1,10), (2,15)]);
+    add_items(&mut orders, 2, &[(2,15)]);
+    add_items(&mut orders, 1, &[(1,5)]);
+
+    println!("{:?}", orders)
 }
