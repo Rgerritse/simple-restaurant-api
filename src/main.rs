c8a14e401f8c38beeff4a89ab628e73a524cc37b
Ruben <rgerritse95@gmail.com>
2022-05-17 13:39:49 +0900

    SharedData to seperate file, and testing done

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod shared_data;
+
+use crate::shared_data::SharedData;
+
 use std::collections::HashMap;
 use std::convert::Infallible;
 use std::net::SocketAddr;
@@ -8,67 +12,35 @@ use hyper::{Body, Request, Response, Server, Method, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 
 use serde_json::Value;
-use rand::Rng;
 
 #[macro_use] extern crate serde_derive;
 
-#[derive(Serialize, Deserialize, Debug)]
-struct Order {
-    order_id: u64,
-    item: String,
-    mins_to_cook: u8,
-}
+#[tokio::main]
+async fn main() {
+    let data = Arc::new(Mutex::new(SharedData { order_counter: 0, tables: HashMap::new() })) ;
 
-#[derive(Debug, Serialize, Deserialize)]
-struct SharedData {
-    order_count: u64,
-    tables: HashMap<u64, Vec<Order>>,
-}
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 
-impl SharedData {
-    fn add_items(&mut self, table: u64, items: Vec<String>) {
-        let mut rng = rand::thread_rng();
-        let mut orders = items.iter().map(|item| {
-            let order = Order {
-                order_id: self.order_count,
-                item: item.to_string(),
-                mins_to_cook:  rng.gen_range(5..16)
-            };
-            self.order_count += 1;
-            return order
-        }).collect::<Vec<Order>>();
-
-        match self.tables.get_mut(&table) {
-            Some(v) => {
-                v.append(&mut orders)
-            }
-            None => {     
-                self.tables.insert(table, orders);  
-            }
-        };
-    }
-
-    fn remove_item(&mut self, table: u64, order_id: u64) -> Option<String> {
-        let orders = self.tables.get_mut(&table);
-
-        let orders = match orders {
-            Some(orders) => orders,
-            None => return Some(String::from("order does not exist"))
-        };
-
-        let index = orders.iter().position(|o| o.order_id == order_id);
-
-        match index {
-            Some(index) => {
-                orders.remove(index);
-                None
-            },
-            None => Some(String::from("order does not exit"))
+    let service = make_service_fn(move |_client: &AddrStream| {
+        let data = data.clone();
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                let data = data.clone();
+                handle_request(req, data)
+            }))
         }
+    });
+
+    let server = Server::bind(&addr).serve(service);
+
+    let graceful = server.with_graceful_shutdown(shutdown_signal());
+
+    if let Err(e) = graceful.await {
+        eprintln!("server error: {}", e);
     }
 }
 
-async fn handle_request(req: Request<Body>, data: Arc<std::sync::Mutex<SharedData>>) -> Result<Response<Body>, hyper::Error> {
+async fn handle_request(req: Request<Body>, data: Arc<Mutex<SharedData>>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
         (&Method::GET, "/items") => {
             let params: HashMap<String, String> = req
@@ -114,31 +86,6 @@ async fn handle_request(req: Request<Body>, data: Arc<std::sync::Mutex<SharedDat
             *not_found.status_mut() = StatusCode::NOT_FOUND;
             Ok(not_found)
         }
-    }
-}
-
-#[tokio::main]
-async fn main() {
-    let data = Arc::new(Mutex::new(SharedData { order_count: 0, tables: HashMap::new() })) ;
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
-
-    let service = make_service_fn(move |_client: &AddrStream| {
-        let data = data.clone();
-        async move {
-            Ok::<_, Infallible>(service_fn(move |req| {
-                let data = data.clone();
-                handle_request(req, data)
-            }))
-        }
-    });
-
-    let server = Server::bind(&addr).serve(service);
-
-    let graceful = server.with_graceful_shutdown(shutdown_signal());
-
-    if let Err(e) = graceful.await {
-        eprintln!("server error: {}", e);
     }
 }
 
---
+++ src/shared_data.rs
@@ -0,0 +1,147 @@
+use std::collections::HashMap;
+
+use rand::Rng;
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Order {
+    order_id: u64,
+    item: String,
+    mins_to_cook: u8,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct SharedData {
+    pub order_counter: u64,
+    pub tables: HashMap<u64, Vec<Order>>,
+}
+
+impl SharedData {
+    pub fn add_items(&mut self, table: u64, items: Vec<String>) {
+        let mut rng = rand::thread_rng();
+        let mut orders = items.iter().map(|item| {
+            let order = Order {
+                order_id: self.order_counter,
+                item: item.to_string(),
+                mins_to_cook:  rng.gen_range(5..16)
+            };
+            self.order_counter += 1;
+            return order
+        }).collect::<Vec<Order>>();
+
+        match self.tables.get_mut(&table) {
+            Some(v) => {
+                v.append(&mut orders)
+            }
+            None => {     
+                self.tables.insert(table, orders);  
+            }
+        };
+    }
+
+    pub fn remove_item(&mut self, table: u64, order_id: u64) -> Option<String> {
+        let orders = self.tables.get_mut(&table);
+
+        let orders = match orders {
+            Some(orders) => orders,
+            None => return Some(String::from("order does not exist"))
+        };
+
+        let index = orders.iter().position(|o| o.order_id == order_id);
+
+        match index {
+            Some(index) => {
+                orders.remove(index);
+                if orders.len() == 0 { self.tables.remove(&table); }
+                None
+            },
+            None => Some(String::from("order does not exit"))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use super::{SharedData, Order};
+
+    #[test]
+    fn test_add_items() {
+        let mut shared_data1 = SharedData { order_counter: 0, tables: HashMap::new() };
+       
+        shared_data1.add_items(1, vec!["pizza".to_string(), "tea".to_string()]);
+
+        let shared_data2 = SharedData {
+            order_counter: 2, 
+            tables: HashMap::from([
+                (1, vec![Order {
+                    order_id: 0,
+                    item: String::from("pizza"),
+                    mins_to_cook: 0
+                }, Order {
+                    order_id: 1,
+                    item: String::from("tea"),
+                    mins_to_cook: 0
+                }]),
+            ]) 
+        };
+
+        check_orders_for_table(&shared_data1, &shared_data2, 1);
+    }
+
+    #[test]
+    fn test_remove_items() {
+        let mut shared_data1 = SharedData {
+            order_counter: 2, 
+            tables: HashMap::from([
+                (1, vec![Order {
+                    order_id: 0,
+                    item: String::from("pizza"),
+                    mins_to_cook: 0
+                }, Order {
+                    order_id: 1,
+                    item: String::from("tea"),
+                    mins_to_cook: 0
+                }]),
+            ]) 
+        };
+
+        shared_data1.remove_item(1, 0);
+
+        let shared_data2 = SharedData {
+            order_counter: 2,
+            tables: HashMap::from([
+                (1, vec![Order {
+                    order_id: 1,
+                    item: String::from("tea"),
+                    mins_to_cook: 0
+                }]),
+            ]) 
+        };
+
+        check_orders_for_table(&shared_data1, &shared_data2, 1);
+
+        shared_data1.remove_item(1, 1);
+        assert!(shared_data1.tables.is_empty(), "tables should be empty! Current length: {}", shared_data1.tables.len());
+    }
+
+    fn check_orders_for_table(shared_data1: &SharedData, shared_data2: &SharedData, table: u64) {
+        assert_eq!(
+            shared_data1.order_counter, 
+            shared_data2.order_counter, 
+            "unequal order counts: {}, {}", 
+            shared_data1.order_counter,
+            shared_data2.order_counter
+        );
+
+        let orders1 = shared_data1.tables.get(&table).unwrap();
+        let orders2 = shared_data2.tables.get(&table).unwrap();
+
+        assert_eq!(orders1.len(), orders2.len(), "unequal lengths: {}, {}", orders1.len(), orders2.len());
+
+        for (order1, order2) in orders1.iter().zip(orders2.iter()) {
+            assert_eq!(order1.order_id, order2.order_id, "different order_ids: {}, {}", order1.order_id, order2.order_id);
+            assert_eq!(order1.item, order2.item, "different items: {}, {}", order1.item, order2.item);
+        }
+    }
+}
